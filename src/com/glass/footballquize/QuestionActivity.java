d015bf32bc21782110641f622498b3e6b8cae457
kAvEh <kaveh.fazaeli@gmail.com>
2014-12-23 18:20:29 +0330

    sdf

--- src/com/glass/footballquize/QuestionActivity.java
+++ src/com/glass/footballquize/QuestionActivity.java
@@ -2,10 +2,24 @@ package com.glass.footballquize;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.glass.utils.AndroidUtils;
+import com.glass.utils.DiffuseFilter;
 
 public class QuestionActivity extends Activity {
+	
+	protected static final int TITLE_TEXT_SIZE = 22;
+
+	protected ImageView mOriginalImageView;
+	protected ImageView mModifyImageView;
+	protected Bitmap mFilterBitmap;
+	
+	private int[] mColors;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +27,35 @@ public class QuestionActivity extends Activity {
 		setContentView(R.layout.activity_question);
 		Intent intent = getIntent();
 		int level = intent.getIntExtra("num", 0);
+		
+		mOriginalImageView = (ImageView) findViewById(R.id.q_main_image_null);
+		mModifyImageView = (ImageView) findViewById(R.id.q_main_image);
+
+		mOriginalImageView.setImageResource(R.drawable.button);
+
+		final int width = mOriginalImageView.getDrawable().getIntrinsicWidth();
+		final int height = mOriginalImageView.getDrawable()
+				.getIntrinsicHeight();
+
+		mColors = AndroidUtils.drawableToIntArray(mOriginalImageView
+				.getDrawable());
+
+		Thread thread = new Thread() {
+			public void run() {
+				DiffuseFilter filter = new DiffuseFilter();
+				filter.setScale(16);
+				mColors = filter.filter(mColors, width, height);
+
+				QuestionActivity.this.runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						setModifyView(mColors, width, height);
+					}
+				});
+			}
+		};
+		thread.setDaemon(true);
+		thread.start();
 	}
 
 	@Override
@@ -20,6 +63,34 @@ public class QuestionActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.question, menu);
 		return true;
+	}
+
+	/**
+	 * ModifyView set
+	 */
+	protected void setModifyView(int[] colors, int width, int height) {
+		mModifyImageView.setWillNotDraw(true);
+
+		if (mFilterBitmap != null) {
+			mFilterBitmap.recycle();
+			mFilterBitmap = null;
+		}
+
+		mFilterBitmap = Bitmap.createBitmap(colors, 0, width, width, height,
+				Bitmap.Config.ARGB_8888);
+		mModifyImageView.setImageBitmap(mFilterBitmap);
+
+		mModifyImageView.setWillNotDraw(false);
+		mModifyImageView.postInvalidate();
+	}
+
+	@Override
+	protected void onDestroy() {
+		if (mFilterBitmap != null) {
+			mFilterBitmap.recycle();
+			mFilterBitmap = null;
+		}
+		super.onDestroy();
 	}
 
 }
